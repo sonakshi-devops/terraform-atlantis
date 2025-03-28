db1f74089a4d7e845260fbffaa25c69686477c6d
sonakshi-devops <sonakshigithub@1234>
2025-03-28 12:47:13 +0530

    new file for feature branch

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,8 +1,17 @@
 provider "aws" {
-  region = "ap-east-1"
+  region = "us-east-1"
 }
 
-resource "aws_s3_bucket-@123" "example" {
-  bucket = "my-atlantis-test-bucket"
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-terraform-bucket-12345"
+  acl    = "private"
 }
 
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "MyTerraformInstance"
+  }
+}
