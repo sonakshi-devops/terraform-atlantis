c1eec4af7c81f42b96d549d37ae0e9c8a9c02a08
sonakshi-devops <sonakshigithub@1234>
2025-03-28 13:21:59 +0530

    feature branch update

--- main.tf
+++ main.tf
@@ -7,6 +7,6 @@ resource "aws_instance" "example" {
   instance_type = var.instance_type
 
   tags = {
-    Name = "Atlantis-Terraform-my-Instance"
+    Name = "Atlantis-Terraform-new-Instance"
   }
 }
