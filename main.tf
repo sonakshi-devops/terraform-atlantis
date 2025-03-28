b39bfb7b7829e02d96a5e55322566cb059aee1a9
sonakshi-devops <sonakshi.srivastava@devddeveloper.in>
2025-03-28 13:26:49 +0530

    reverted main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "example-1" {
   ami           = var.ami_id
   instance_type = var.instance_type
 
