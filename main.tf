2bd05c89a477bfb3571ed1217fd2fd5572c692da
sonakshi-devops <sonakshi.srivastava@devddeveloper.in>
2025-03-28 14:02:21 +0530

    updated my main.tf file

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "example-1" {
+resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
 
